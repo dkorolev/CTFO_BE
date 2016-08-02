221155f6c4f2dc04bfbf84d1291899d75c039f3f
Dima <dmitry.korolev@gmail.com>
2016-08-02 01:36:03 -0700

    iOS devices table import.

--- 2016_08_01_conversion/convert.cc
+++ 2016_08_01_conversion/convert.cc
@@ -52,56 +52,57 @@ CURRENT_TYPE_EVOLVER(Evolver_2016_08_01,
                        CURRENT_NATURAL_EVOLVE(NewCTFOStorage, CTFO_2016_08_01, from.mutations, into.mutations);
                      });
 
-struct TypesCounter {
-  std::map<std::string, size_t> counters;
-  template <typename T>
-  void operator()(const T&) {
-    ++counters[current::reflection::CurrentTypeName<T>()];
+struct CTFOEventsParser {
+  std::chrono::microseconds us;
+  std::vector<std::pair<std::chrono::microseconds, std::string>>& output;
+
+  CTFOEventsParser(std::vector<std::pair<std::chrono::microseconds, std::string>>& output) : output(output) {}
+
+  void operator()(const current::midichlorians::ios::iOSDeviceInfo& input_device_info) {
+    typename CTFO_2016_08_01::CTFOEvent output_event;
+    output_event.meta.begin_us = us + std::chrono::microseconds(1);
+    output_event.meta.end_us = us + std::chrono::microseconds(3);
+
+    typename CTFO_2016_08_01::iOSDeviceInfo output_device_info;
+
+    typename CTFO_2016_08_01::Device output_device;
+    output_device.did = input_device_info.device_id;
+    static const std::string field_name = "advertisingIdentifier";
+    output_device.ad_id = input_device_info.info.count(field_name) ? input_device_info.info.at(field_name) : "";
+
+    current::type_evolution::Evolve<CTFO_2016_08_01,
+                                    typename CTFO_2016_08_01::iOSDeviceInfo,
+                                    current::type_evolution::Evolver_2016_08_01>::
+        template Go<CTFO_2016_08_01>(
+            reinterpret_cast<const typename CTFO_2016_08_01::iOSDeviceInfo&>(input_device_info),
+            output_device.info);
+
+    typename CTFO_2016_08_01::Persisted_DeviceUpdated output_device_info_mutation_event;
+    output_device_info_mutation_event.us = us + std::chrono::microseconds(1);
+    output_device_info_mutation_event.data = output_device;
+    output_event.mutations.push_back(output_device_info_mutation_event);
+
+    output.emplace_back(us, JSON(output_event));
   }
+
+  // Ignore all events except `current::midichlorians::ios::iOSDeviceInfo` for now.
+  template <typename T>
+  void operator()(const T&) {}
+
 };
 
 int main(int argc, char** argv) {
   ParseDFlags(&argc, &argv);
 
-  if (false) {
-    TypesCounter counter;
-    for (const std::string& line : current::strings::Split<current::strings::ByLines>(
-             current::FileSystem::ReadFileAsString(FLAGS_old_ctfo_events))) {
-      const auto e = ParseJSON<current::midichlorians::server::EventLogEntry, JSONFormat::Minimalistic>(line);
-      e.event.Call(counter);
-    }
-    std::cout << JSON(counter.counters);
-    return 0;
-    /*
-      $ cat ctfo_events.log | sed 's/""/"NULL"/g' | jq -r .event.NULL | sort | uniq -c | sort -gr
-        68165 T9201174724793298462
-         8495 T9207776099720188653
-         1171 T9201594862172611068
-         1165 T9205653292032376105
-          266 T9204032455253812491
-            3 T9202274666560225185
+  std::vector<std::pair<std::chrono::microseconds, std::string>> output;
 
-      T9201174724793298462: iOSGenericEvent
-      T9207776099720188653: iOSFocusEvent
-      T9201594862172611068: iOSDeviceInfo
-      T9205653292032376105: iOSAppLaunchEvent
-      T9204032455253812491: iOSFirstLaunchEvent
-      T9202274666560225185: (a dummy event with fake_did, ignore those three occurrences)
-
-      $ ./.current/convert | jq .
-      {
-        "iOSGenericEvent": 68168,
-        "iOSFocusEvent": 8495,
-        "iOSFirstLaunchEvent": 266,
-        "iOSDeviceInfo": 1171,
-        "iOSAppLaunchEvent": 1165
-      }
-
-      => OK to leave this way, with `JSONFormat::Minimalistic`. -- D.K.
-    */
+  CTFOEventsParser parser(output);
+  for (const std::string& line : current::strings::Split<current::strings::ByLines>(
+           current::FileSystem::ReadFileAsString(FLAGS_old_ctfo_events))) {
+    const auto e = ParseJSON<current::midichlorians::server::EventLogEntry, JSONFormat::Minimalistic>(line);
+    parser.us = e.server_us;
+    e.event.Call(parser);
   }
-
-  std::vector<std::string> output;
 
   for (const std::string& line : current::strings::Split<current::strings::ByLines>(
            current::FileSystem::ReadFileAsString(FLAGS_old_db))) {
@@ -111,14 +112,32 @@ int main(int argc, char** argv) {
     typename NewCTFOStorage::TopLevelTransaction from;
     typename CTFO_2016_08_01::CTFOEvent into;
 
+    const auto idx_ts = ParseJSON<idxts_t>(parts[0]);
     ParseJSON(parts[1], from);
+
     current::type_evolution::Evolve<
         NewCTFOStorage,
         typename NewCTFOStorage::TopLevelTransaction,
         current::type_evolution::Evolver_2016_08_01>::template Go<CTFO_2016_08_01>(from, into);
 
-    output.push_back(parts[0] + '\t' + JSON(into));
+    output.emplace_back(idx_ts.us, JSON(into));
   }
 
-  current::FileSystem::WriteStringToFile(current::strings::Join(output, '\n'), FLAGS_new_db.c_str());
+  std::sort(output.begin(), output.end());
+
+  {
+    std::ofstream fo(FLAGS_new_db);
+    idxts_t key;
+    key.index = 0;
+    std::chrono::microseconds previous_timestamp;
+    for (const auto& e : output) {
+      assert(e.first > previous_timestamp);
+      previous_timestamp = e.first;
+
+      key.us = e.first;
+      fo << JSON(key) << '\t' << e.second << '\n';
+
+      ++key.index;
+    }
+  }
 }
