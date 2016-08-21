7d56f234188ad13b6234d8e54353dd7e0b06b47a
Grigory Nikolaenko <nikolaenko.grigory@gmail.com>
2016-08-22 00:42:23 +0300

    Fixed indentation in the CTFO crunches sources.

--- crunchers/cruncher.h
+++ crunchers/cruncher.h
@@ -1,19 +1,19 @@
 /*******************************************************************************
  The MIT License (MIT)
- 
+
  Copyright (c) 2016 Grigory Nikolaenko <nikolaenko.grigory@gmail.com>
- 
- 
+
+
  Permission is hereby granted, free of charge, to any person obtaining a copy
  of this software and associated documentation files (the "Software"), to deal
  in the Software without restriction, including without limitation the rights
  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  copies of the Software, and to permit persons to whom the Software is
  furnished to do so, subject to the following conditions:
- 
+
  The above copyright notice and this permission notice shall be included in all
  copies or substantial portions of the Software.
- 
+
  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -37,17 +37,17 @@ struct EntryCruncherImpl : public IMPL {
   using EntryResponse = current::ss::EntryResponse;
   using TerminationResponse = current::ss::TerminationResponse;
   using entry_t = typename IMPL::entry_t;
-	
+
   template <typename... ARGS>
   EntryCruncherImpl(ARGS&&... args)
-    : IMPL(std::forward<ARGS>(args)...) {}
+      : IMPL(std::forward<ARGS>(args)...) {}
   virtual ~EntryCruncherImpl() {}
 
   EntryResponse operator()(const entry_t& entry, idxts_t current, idxts_t) {
     IMPL::OnEvent(entry, current);
     return EntryResponse::More;
   }
-	
+
   EntryResponse EntryResponseIfNoMorePassTypeFilter() const { return EntryResponse::More; }
   TerminationResponse Terminate() const { return TerminationResponse::Terminate; }
 };
