79fb387c04fbb4ca360cc50701d3213841ae4ffe
Grigory Nikolaenko <nikolaenko.grigory@gmail.com>
2016-08-23 00:20:29 +0300

    Fixed splicing to the wrong place in the ActiveUsersCruncher.

--- crunchers/cruncher_active_users.h
+++ crunchers/cruncher_active_users.h
@@ -26,10 +26,11 @@
 #ifndef CRUNCHER_ACTIVE_USERS_H
 #define CRUNCHER_ACTIVE_USERS_H
 
-#include "cruncher.h"
-#include "schema.h"
 #include <list>
 #include <unordered_map>
+
+#include "cruncher.h"
+#include "schema.h"
 
 namespace CTFO {
 
@@ -37,19 +38,25 @@ template <typename NAMESPACE>
 struct ActiveUsersCruncherImpl {
   using entry_t = typename NAMESPACE::CTFOLogEntry;
 
-  ActiveUsersCruncherImpl(std::chrono::seconds interval) : interval_(interval), skip_(0) {}
+  ActiveUsersCruncherImpl(std::chrono::microseconds interval) : interval_(interval), stream_size_(0) {}
   virtual ~ActiveUsersCruncherImpl() = default;
 
-  void OnIOSEvent(const typename NAMESPACE::iOSBaseEvent& e) {
-    const auto cit = users_map_.find(e.device_id);
-    if (cit != users_map_.end()) {
-      if (cit->second != users_list_.begin()) users_list_.splice(users_list_.begin(), users_list_, cit->second);
-      cit->second->us = current_time_;
-    } else {
-      users_list_.push_back(ActiveUser{current_time_, e.device_id});
-      auto it = users_list_.end();
-      users_map_[e.device_id] = --it;
+  void OnEvent(const entry_t& e, idxts_t idxts) {
+    current_us_ = idxts.us;
+    ++stream_size_;
+    e.Call(*this);
+    while (!users_list_.empty() && users_list_.back().us + interval_ < current_us_) {
+      users_map_.erase(users_list_.back().device_id);
+      users_list_.pop_back();
     }
+  }
+
+  uint64_t Count() const { return users_list_.size(); }
+  uint64_t Size() const { return stream_size_; }
+
+  void operator()(const typename NAMESPACE::EventLogEntry& e) {
+    current_us_ = e.server_us;
+    e.event.Call(*this);
   }
 
   void operator()(const typename NAMESPACE::iOSGenericEvent& e) { OnIOSEvent(e); }
@@ -62,41 +69,35 @@ struct ActiveUsersCruncherImpl {
 
   void operator()(const typename NAMESPACE::iOSFirstLaunchEvent& e) { OnIOSEvent(e); }
 
-  void operator()(const typename NAMESPACE::EventLogEntry& e) {
-    current_time_ = e.server_us;
-    e.event.Call(*this);
-  }
-
   template <typename ANYTHING>
-  void operator()(const ANYTHING&) {
-    ++skip_;
-  }
-
-  void OnEvent(const entry_t& e, idxts_t idxts) {
-    current_time_ = idxts.us;
-    e.Call(*this);
-    while (!users_list_.empty() && users_list_.front().us + interval_ < current_time_) {
-      users_map_.erase(users_list_.front().device_id);
-      users_list_.pop_front();
-    }
-  }
-
-  uint64_t Count() const { return users_list_.size(); }
-  uint64_t Skip() const { return skip_; }
+  void operator()(const ANYTHING&) {}
 
  private:
-  struct ActiveUser {
+  struct ActiveUser final {
     std::chrono::microseconds us;
     std::string device_id;
   };
   using user_list_t = std::list<ActiveUser>;
   using user_map_t = std::unordered_map<std::string, typename user_list_t::iterator>;
 
+  void OnIOSEvent(const typename NAMESPACE::iOSBaseEvent& e) {
+    const auto cit = users_map_.find(e.device_id);
+    if (cit != users_map_.end()) {
+      if (cit->second != users_list_.begin()) {
+        users_list_.splice(users_list_.begin(), users_list_, cit->second);
+      }
+      cit->second->us = current_us_;
+    } else {
+      users_list_.push_front(ActiveUser{current_us_, e.device_id});
+      users_map_[e.device_id] = users_list_.begin();
+    }
+  }
+
   user_list_t users_list_;
   user_map_t users_map_;
-  std::chrono::microseconds current_time_;
-  std::chrono::seconds interval_;
-  uint64_t skip_;
+  std::chrono::microseconds current_us_;
+  std::chrono::microseconds interval_;
+  uint64_t stream_size_;
 };
 
 template <typename NAMESPACE>
